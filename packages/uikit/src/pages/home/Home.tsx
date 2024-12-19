711f0b9f9ddd0cb763063f8e81a4298488f91455
dejoy <dejoy@172-10-40-198.lightspeed.austtx.sbcglobal.net>
2024-12-20 03:12:45 +0800

    rm deprecated

--- packages/uikit/src/pages/home/Home.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@nextui-org/react';
-
-export function Home() {
-    return (
-        <Button color="warning" className="mt-4">
-            Demo
-        </Button>
-    );
-}
